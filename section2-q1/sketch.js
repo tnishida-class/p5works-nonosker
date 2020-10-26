3e6306186ddc8b96c9f266c00a9887f3c0acce6d
nonosker <65202742+nonosker@users.noreply.github.com>
2020-10-26 14:11:39 +0900

    section2-q1-update
    小手調べ

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,14 @@
 // 小手調べ
 function setup(){
+  noFill()
   for(let i = 0; i < 10; i++){
-    // BLANK[1]
+    let s=(i+1)*10
+    if(i<5){
+      stroke(0,0,255);
+    }
+    else{
+      stroke(255,0,0);
+    }
+    ellipse(50,50,s,s);
   }
 }
