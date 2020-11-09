64356875b85ed022d020a3f2b57c0628dd570358
nonosker <65202742+nonosker@users.noreply.github.com>
2020-11-10 05:19:44 +0900

    update-section2-q2

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -6,10 +6,10 @@ noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
       if((i+j)%2==0){
-      fill(0);
+      fill(255);
     }
     else{
-      fill(255);
+      fill(196);
     }
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
       rect(size*i,size*j,size,size);
