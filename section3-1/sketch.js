7255c6f8dbb51dbed4219d2e24d9cad15f5e1acd
nonosker <65202742+nonosker@users.noreply.github.com>
2020-11-10 06:27:08 +0900

    update-section3-1

--- section3-1/sketch.js
+++ section3-1/sketch.js
@@ -1,12 +1,24 @@
 // テキスト「関数を使う」
 // 練習問題：このプログラムを改造してEUの旗を描いてみよう
 function setup(){
-  createCanvas(200, 200);
-  background(192);
+  createCanvas(300, 200);
+  background(0,51,153);
   for(let i = 0; i < 12; i++){
-    let theta = TWO_PI * i / 12;
-    let x = 100 + cos(theta) * 50;
-    let y = 100 + sin(theta) * 50;
-    ellipse(x, y, 10);
+    const theta = TWO_PI * i / 12;
+    const x = 150 + cos(theta) * 50;
+    const y = 100 + sin(theta) * 50;
+    fill(255,204,0);
+    noStroke();
+    star(x, y, 10);
   }
+}
+function star(cx,cy,r){
+  beginShape();
+  for(var i=0; i<5; i++){
+    let theta=TWO_PI*i*2/5-HALF_PI;
+    let x=cx+cos(theta)*r;
+    let y=cy+sin(theta)*r;
+    vertex(x,y);
+  }
+  endShape(CLOSE);
 }
