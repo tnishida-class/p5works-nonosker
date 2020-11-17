a1f7117b8606ecd7aec00ec0c602a28aae1215bf
nonosker <65202742+nonosker@users.noreply.github.com>
2020-11-17 20:40:02 +0900

    Update-section-q2

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -9,10 +9,22 @@ noStroke();
       fill(255);
     }
     else{
-      fill(196);
+      fill(128);
     }
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
       rect(size*i,size*j,size,size);
     }
+    }
+    for(let i=0; i<8; i++){
+      for(let j=0; j<8; j++){
+        if(((i+j)%2!=0)&&j<3){
+          fill(255,0,0);
+          ellipse(i*size+size/2,j*size+size/2,9*size/10)
+        }
+        if(((i+j)%2!=0)&&j>4){
+          fill(0);
+          ellipse(i*size+size/2,j*size+size/2,9*size/10)
+        }
+      }
+    }
   }
-}
